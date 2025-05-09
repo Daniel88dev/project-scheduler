69ad0cf533c49b9647921d2f536ef5a173810b33
Daniel88dev <64728456+Daniel88dev@users.noreply.github.com>
2025-05-09 22:04:58 +0200

    Added date picker to MilestoneChange Dialog

--- src/components/timeline-components/MilestoneChange.tsx
+++ src/components/timeline-components/MilestoneChange.tsx
@@ -12,6 +12,18 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog.tsx";
 import { useForm } from "@tanstack/react-form";
+import { Input } from "@/components/ui/input.tsx";
+import { Label } from "@/components/ui/label.tsx";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover-dialog.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { cn } from "@/lib/utils.ts";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar.tsx";
 
 interface MilestoneChangeProps {
   milestoneData: Milestone;
@@ -37,26 +49,7 @@ const MilestoneChange = ({
       onMilestoneClose();
       setOpen(false);
     },
-    // onSubmitError: (errors) => {
-    //   console.log(errors);
-    // },
-    // onSubmitSuccess: (values) => {
   });
-  //
-  // const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   setMilestone((prevState) => {
-  //     return {
-  //       ...prevState,
-  //       name: e.target.value,
-  //     };
-  //   });
-  // };
-  //
-  // const onSubmitButtonClick = () => {
-  //   onMilestoneSubmit(milestone);
-  //   onMilestoneClose();
-  //   setOpen(false);
-  // };
 
   return (
     <Dialog
@@ -70,14 +63,76 @@ const MilestoneChange = ({
         <DialogHeader>
           <DialogTitle>Change Milestone</DialogTitle>
         </DialogHeader>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          form.handleSubmit();
-        }}
-        {/*<p>{milestone.name}</p>*/}
-        {/*<input type={"text"} value={milestone.name} onChange={onNameChange} />*/}
-        {/*<Button onClick={onSubmitButtonClick}>Save</Button>*/}
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            await form.handleSubmit();
+          }}
+          className="flex flex-col gap-4"
+        >
+          {/*Name input field*/}
+          <form.Field
+            name={"name"}
+            children={(field) => {
+              return (
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor={field.name}>Enter Milestone Name:</Label>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    type={"text"}
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                  {!field.state.meta.isValid &&
+                    field.state.meta.errors.length > 0 && (
+                      <em role="alert">
+                        {field.state.meta.errors
+                          .map((error) => error!.message)
+                          .join(", ")}
+                      </em>
+                    )}
+                </div>
+              );
+            }}
+          />
+          {/*Date picker field*/}
+          <form.Field
+            name={"date"}
+            children={(field) => {
+              return (
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[280px] justify-start text-left font-normal",
+                        !field.state.value && "text-muted-foreground"
+                      )}
+                    >
+                      <CalendarIcon className="mr-2 h-4 w-4" />
+                      {field.state.value ? (
+                        format(field.state.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0">
+                    <Calendar
+                      mode="single"
+                      selected={field.state.value}
+                      onSelect={(e) => {
+                        if (e) field.handleChange(e);
+                      }}
+                    />
+                  </PopoverContent>
+                </Popover>
+              );
+            }}
+          />
+        </form>
       </DialogContent>
     </Dialog>
   );
